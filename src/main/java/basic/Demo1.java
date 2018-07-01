9a3d0f45f85c02be5eca576dbd467684581d25d8
chong <fqyyang@163.com>
2018-06-30 22:02:48 -0700

    serialize/deserialize with local file

---
+++ src/main/java/basic/Demo1.java
@@ -0,0 +1,75 @@
+package basic;
+
+import com.fqyang.AddressBookProtos;
+
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+public class Demo1 {
+
+    public static void main(String args[]){
+        //ConstructAndSerializeToLocalFile();
+        DeserializeFromFile();
+        System.out.println("done");
+    }
+
+    //construct a Person object, and then serialize it.
+    private static void ConstructAndSerializeToLocalFile(){
+        AddressBookProtos.Person.Builder person =  AddressBookProtos.Person.newBuilder();
+
+        person.setId(1);
+        person.setName("Lilei");
+        person.setEmail("fqyang@163.com");
+
+        AddressBookProtos.Person.PhoneNumber.Builder MobileNumber =
+                AddressBookProtos.Person.PhoneNumber.newBuilder().setNumber("4099134756");
+        MobileNumber.setType(AddressBookProtos.Person.PhoneType.MOBILE);
+        person.addPhones(MobileNumber);
+
+        AddressBookProtos.Person.PhoneNumber.Builder HomeNumber =
+                AddressBookProtos.Person.PhoneNumber.newBuilder().setNumber("10086");
+        HomeNumber.setType(AddressBookProtos.Person.PhoneType.HOME);
+        person.addPhones(HomeNumber);
+
+        FileOutputStream output = null;
+        try {
+            output = new FileOutputStream("C:\\D_study\\Protobuf\\exercise\\demo1.txt");
+            person.build().writeTo(output);
+            output.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static void DeserializeFromFile(){
+        String MessageFile = "C:\\D_study\\Protobuf\\exercise\\demo1.txt";
+        // Read the existing address book.
+        try {
+            AddressBookProtos.Person person =
+                    AddressBookProtos.Person.parseFrom(new FileInputStream(MessageFile));
+
+            System.out.println("Person ID: " + person.getId());
+            System.out.println("Name: " + person.getName());
+            System.out.println("Email: " + person.getEmail());
+
+            for (AddressBookProtos.Person.PhoneNumber phoneNumber : person.getPhonesList()) {
+                switch (phoneNumber.getType()) {
+                    case MOBILE:
+                        System.out.print("  Mobile phone #: ");
+                        break;
+                    case HOME:
+                        System.out.print("  Home phone #: ");
+                        break;
+                    case WORK:
+                        System.out.print("  Work phone #: ");
+                        break;
+                }
+                System.out.println(phoneNumber.getNumber());
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
